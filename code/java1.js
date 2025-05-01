8113cf63aec27a3e946d9d593523963cb6778abe
JOHNPORK1212 <cobrinquin@gmail.com>
2025-05-01 19:22:39 +0200

    Update and rename server.js to java1.js

---
+++ code/java1.js
@@ -0,0 +1,32 @@
+function validerEtRediriger() {
+    var pseudo = document.getElementById('pseudoInput').value;
+  
+
+    if (pseudo.trim() === "") {
+      afficherAlerte("entre un pseudo zbi");
+      return;
+    }
+  
+
+    localStorage.setItem('pseudo', pseudo);
+  
+
+    window.location.href = 'sallejeu.html';
+  }
+  
+  function afficherAlerte(message) {
+    document.getElementById('alerteMessage').innerText = message;
+  
+    document.getElementById('alerte').style.display = 'block';
+  }
+  
+  function fermerAlerte() {
+    document.getElementById('alerte').style.display = 'none';
+  }
+  
+  window.onload = function () {
+    var pseudo = localStorage.getItem('pseudo');
+    if (pseudo) {
+      document.getElementById('affichagePseudo').innerText = "Bienvenue, " + pseudo + "!";
+    }
+  };
